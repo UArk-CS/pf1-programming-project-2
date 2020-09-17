b515674c6afde323ccf9c31897ae30cb2555f777
Benjamin Worthington <benworthin@icloud.com>
2020-09-16 22:30:41 -0500

    Implemented the Sorting Hat Simulation
    Added the sorting hat simulation and documented the code.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,17 @@
 #include <algorithm>
 using namespace std;
 
-// Galleon in terms of sickles
+//Define constants
 const int GALLEON = 17;
+const string GRYFFINDOR = "Gryffindor";
+const string HUFFLEPUFF = "Hufflepuff";
+const string SLYTHERIN = "Slytherin";
+const string RAVENCLAW = "Ravenclaw";
 
 void woodMenuOutput() {
+
+    // Stores output for the wand wood menu
+    // In a function so it can be called again when validating user input rather than rewriting all the cout statements
 
     cout << "Select one of our fine woods for your wand:" << endl;
     cout << "\tBLACK WALNUT\n\t\tA sleek dark finish meant for only those with good instinct\n\t\t10 sickles/inch" << endl;
@@ -18,6 +25,9 @@ void woodMenuOutput() {
 
 void coreMenuOutput() {
 
+    // Stores the output for the wand core menu
+    // In a function so it can be called again when validating user input rather than rewriting all the cout statements
+
     cout << "Select one of our powerful wand cores:" << endl;
     cout << "\tDRAGON HEARTSTRING\n\t\tWhile it can be temperamental, this core is the most powerful and can learn the quickest\n\t\t15 sickles/inch" << endl;
     cout << "\tPHOENIX FEATHER\n\t\tThe rarest core there is. It can be tricky to bond with, but it is also the most capable across all magic types\n\t\t20 sickles/inch" << endl;
@@ -27,12 +37,17 @@ void coreMenuOutput() {
 
 void wandLengthMenuOutput() {
 
+    // Stores the output for the wand length menu
+    // In a function so it can be called again when validating user input rather than rewriting all the cout statements
+
     cout << "Now all that is left, what length would you like your wand? (between 8-15 inches)" << endl;
     cout << "!!! Whole numbers only. !!!" << endl;
 
 }
 
 string getUserInput() {
+
+    // Gets the user input and transforms the string to all uppercase to later compare when validating the input
 
     cout << "\n** Please type your selection as seen above **" << endl;
 
@@ -49,22 +64,29 @@ string getUserInput() {
 
 bool validateUserInput(const string& userInput, map<string, int> validInput) {
 
-   map<string, int>::iterator it;
+    // Validates the user input by comparing their input to the choices predefined in the map
 
-   for(it = validInput.begin(); it != validInput.end(); ++it) {
-       string validChoice = it->first;
+    // Creates an iterator for the map
+    map<string, int>::iterator it;
 
-       if(validChoice == userInput) {
-           return true;
-       }
-   }
+    // Loops through the map using the iterator to see if the users input matches any valid input
+    for(it = validInput.begin(); it != validInput.end(); ++it) {
+        string validChoice = it->first;  // Looks at the key in the map, not the value
 
-   return false;
+        if(validChoice == userInput) {
+            return true;
+        }
+    }
+
+    return false;
 
 }
 
 void wandShopSimulation() {
 
+    // !! STORES THE CODE FOR ENTIRE WAND SHOP SIMULATION !!
+
+    // while loop controller
     bool loopAgain = true;
 
     // Map for the wand word choices (in Sickles)
@@ -93,6 +115,10 @@ void wandShopSimulation() {
     cout << "Welcome to Professor Worthington's Quality Wand Shop" << endl;
     cout << "Let us see if we can find the wand for you.\n"  << endl;
 
+    // Outputs the menu, gets users input, validates the input, then inside the while loop if validateUserInput()
+    // returned true, the loop breaks and moves on. Otherwise, the program prompts the user that they entered
+    // invalid input and reprints the menu, gets new data, and re-validates it. Continues until valid input.
+    // Repeated for wood, core, and length of wand.
     woodMenuOutput();
     string usersWoodChoice = getUserInput();
     bool isValid = validateUserInput(usersWoodChoice, wandWoodChoices);
@@ -137,19 +163,126 @@ void wandShopSimulation() {
         }
     }
 
+    // Calculates the wand price, then converts it to Galleons and Sickles
     int wandPrice = (wandWoodChoices.at(usersWoodChoice) + wandCoreChoices.at(usersCoreChoice)) * wandLengthChoices.at(wandLength);
     int wandPriceGalleons = wandPrice / GALLEON;
     int wandPriceSickles = wandPrice % GALLEON;
 
+    // Outputs the users choices and the price of their wand
     cout << "Ahh yes, a " << wandLength << " inch " << usersWoodChoice << " wood wand with a " << usersCoreChoice << " core will be very powerful in the right hands." << endl;
     cout << "That's going to be " << wandPriceGalleons << " galleons & " << wandPriceSickles << " sickles (" << wandPrice << " sickles) youngin'." << endl;
-    cout << "Thank you for your business! With great power comes great responsibility, so don't disappoint youngin'!" << endl;
+    cout << "Thank you for your business! With great power comes great responsibility, so don't disappoint! Now off you go to Hogwarts!" << endl;
+
+}
+
+void questionOneMenuOutput() {
+
+    // Stores the output for the first question
+    // In a function so it can be called again when validating user input rather than rewriting all the cout statements
+
+    cout << "1. If you could make a potion that could guarantee you one thing, what would it be?" << endl;
+    cout << "\tA. Love" << endl;
+    cout << "\tB. Wisdom" << endl;
+
+}
+
+void questionTwoMenuOutput() {
+
+    // Stores the output for the second question
+    // In a function so it can be called again when validating user input rather than rewriting all the cout statements
+
+    cout << "2. When I am dead, I want people to remember me as:" << endl;
+    cout << "\tA. The Great" << endl;
+    cout << "\tB. The Wise" << endl;
+
+}
+
+void questionThreeMenuOutput() {
+
+    // Stores the output for the third question
+    // In a function so it can be called again when validating user input rather than rewriting all the cout statements
+
+    cout << "3. What would you least liked to be called?" << endl;
+    cout << "\tA. Cowardly" << endl;
+    cout << "\tB. Selfish" << endl;
+
+}
+
+void hogwartsSortingHatSimulation() {
+
+    bool loopAgain = true;  // while loop controller
+    string usersHouseAssignment;
+
+    cout << "Welcome to Hogwarts young wizards and witches!" << endl;
+    cout << "We will now begin the House Sorting ceremony to place you into your rightful house." << endl;
+    cout << "Please answer the following questions with either A or B. Good luck!\n" << endl;
+
+    // Outputs the question and answer choices, gets the users input, and then checks to see if the answer is valid
+    // Repeated for question two and three
+    questionOneMenuOutput();
+    string questionOneAnswer = getUserInput();
+    while(loopAgain) {
+        if((questionOneAnswer == "A") || (questionOneAnswer == "B")) {
+            loopAgain = false;
+        } else {
+            cout << "!!! Invalid input. Please try again with a valid answer. !!!\n" << endl;
+            questionOneMenuOutput();
+            questionOneAnswer = getUserInput();
+        }
+    }
+
+    loopAgain = true;
+    questionTwoMenuOutput();
+    string questionTwoAnswer = getUserInput();
+    while(loopAgain) {
+        if((questionTwoAnswer == "A") || (questionTwoAnswer == "B")) {
+            loopAgain = false;
+        } else {
+            cout << "!!! Invalid input. Please try again with a valid answer. !!!\n" << endl;
+            questionTwoMenuOutput();
+            questionTwoAnswer = getUserInput();
+        }
+    }
+
+    loopAgain = true;
+    questionThreeMenuOutput();
+    string questionThreeAnswer = getUserInput();
+    while(loopAgain) {
+        if((questionThreeAnswer == "A") || (questionThreeAnswer == "B")) {
+            loopAgain = false;
+        } else {
+            cout << "!!! Invalid input. Please try again with a valid answer. !!!\n" << endl;
+            questionThreeMenuOutput();
+            questionThreeAnswer = getUserInput();
+        }
+    }
+
+    // Determines the users House based on the constraints listed in the PDF then assigns it to userHouseAssignment
+    if(questionOneAnswer == "A") {
+       if(questionThreeAnswer == "A") {
+           usersHouseAssignment = GRYFFINDOR;
+       } else if(questionThreeAnswer == "B") {
+           usersHouseAssignment = HUFFLEPUFF;
+       }
+    } else if(questionOneAnswer == "B") {
+        if(questionTwoAnswer == "A") {
+            usersHouseAssignment = SLYTHERIN;
+        } else if(questionTwoAnswer == "B") {
+            usersHouseAssignment = RAVENCLAW;
+        }
+    }
+
+    cout << "The sorting hat chose... " << usersHouseAssignment << "! You'll certainly fit in there!" << endl;
+    cout << "Enjoy your stay at Hogwarts!" << endl;
 
 }
 
 int main() {
 
+    // Calls the wand shop function and the sorting hat function
     wandShopSimulation();
+    cout << endl;
+    hogwartsSortingHatSimulation();
 
     return 0;
 
